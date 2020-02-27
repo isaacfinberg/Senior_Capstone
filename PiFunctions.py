bfbe8f31f63ef805a99ea9481a9fb4489a334ae4
Isaac Finberg <isaacfinberg@gwu.edu>
2020-02-27 15:37:46 -0500

    New test script for servos and buttons using pigpio to stop servo jitter: works

--- PiFunctions.py
+++ PiFunctions.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 import pygame
 from subprocess import Popen
@@ -6,25 +7,28 @@ import os
 import sys
 from PIL import Image
 
-def SetAngleDoor( angle, p1, p2 ):
-    for i in range( 0, angle ):
-        dutyRatioL = i / 18 + 2
-        dutyRatioR = ( angle - i ) / 18 + 2
-        p1.ChangeDutyCycle( dutyRatioL )
-        p2.ChangeDutyCycle( dutyRatioR )
-        sleep( .01 )
+pi = pigpio.pi()
+
+def SetAngleDoor( pw1, servo1, servo2 ):
+    a1 = pi.get_servo_pulsewidth( servo1 )
+    a2 = pi.get_servo_pulsewidth( servo2 )
+    j = 0
+    for i in range( a1, pw1 ):
+        pi.set_servo_pulsewidth( servo1, i )
+        pi.set_servo_pulsewidth( servo2, a2 - j )
+        j += 1
+        sleep( .001 )
     
-def SetAngleWheel( angle, p, state ):
-    if state == 'closed':
-        for i in range( 0, angle ):
-            dutyRatio = i / 18 + 2
-            p.ChangeDutyCycle( dutyRatio )
-            sleep( .01 )
+def SetAngleWheel( pw, servo ):
+    a = pi.get_servo_pulsewidth( servo )
+    if a == 1500:
+        for i in range( a, pw ):
+            pi.set_servo_pulsewidth( servo, i )
+            sleep( .001 )
     else:
-        for i in range( 0, angle ):
-            dutyRatio = ( angle - i ) / 18 + 2
-            p.ChangeDutyCycle( dutyRatio )
-            sleep( .01 )          
+        for i in range( abs( a - pw ) ):
+            pi.set_servo_pulsewidth( servo, a - i )
+            sleep( .001 )          
     
 def PlayAudio( audio_file ):
     pygame.mixer.init()
@@ -40,44 +44,45 @@ def DisplayImg( image_file ):
     im = Image.open( image_file )
     im.show()
     
-def three_one( p1, p2, sound_high, state, image_file ):
-    angle = 90
-    SetAngleDoor( angle, p1, p2 )
-    SetAngleWheel( angle, p, state )
+def three_one( prim1, prim2, wheel, sound_high, image_file ):
+    pw1 = 2300
+    pw = pw1
+    SetAngleDoor( pw1, prim1, prim2 )
+    SetAngleWheel( pw, wheel )
     #DisplayImg( image_file )
     #PlayAudio( sound_high )
 
-def one_two( p1, p2, sound_low, image_file ):
-    angle = 90
-    SetAngleDoor( angle, p1, p2 )
+def one_two( sec1, sec2, sound_low, image_file ):
+    pw1 = 2300
+    SetAngleDoor( pw1, sec1, sec2 )
     #DisplayImg( image_file )
     #PlayAudio( sound_low )
     
-def two_one( p1, p2, sound_high, image_file ):
-    angle = 90
-    SetAngleDoor( angle, p2, p1 )
+def two_one( sec1, sec2, sound_high, image_file ):
+    pw1 = 1500
+    SetAngleDoor( pw1, sec2, sec1 )
     #DisplayImg( image_file )
     #PlayAudio( sound_high )
     
-def two_three( p1, p2, p3, p4, p5, sound_neutral, state, image_file ):
-    angle = 90
-    SetAngleDoor( angle, p2, p1 )
-    SetAngleWheel( angle, p5, state )
-    SetAngleDoor( angle, p2, p1 )
+def two_three( prim1, prim2, sec1, sec2, wheel, sound_neutral, image_file ):
+    pw1 = 1500
+    SetAngleDoor( pw1, sec2, sec1 )
+    SetAngleWheel( pw1, wheel )
+    SetAngleDoor( pw1, prim2, prim1 )
     #DisplayImg( image_file )
     #PlayAudio( sound_neutral )
     
-def three_two( p1, p2, p3, p4, p5, sound_low, state, image_file ):
-    angle = 90
-    SetAngleDoor( angle, p1, p2 )
-    SetAngleWheel( angle, p5, state )
-    SetAngleDoor( angle, p3, p4 )
+def three_two( prim1, prim2, sec1, sec2, wheel, sound_low, image_file ):
+    pw1 = 2300
+    SetAngleDoor( pw1, prim1, prim2 )
+    SetAngleWheel( pw1, wheel )
+    SetAngleDoor( pw1, sec1, sec2 )
     #DisplayImg( image_file )
     #PlayAudio( sound_low )
     
-def one_three( p1, p2, p3, sound_neutral, state, image_file ):
-    angle = 90
-    SetAngleWheel( angle, p3, state )
-    SetAngleDoor( angle, p2, p1 )
+def one_three( prim1, prim2, wheel, sound_neutral, image_file ):
+    pw1 = 1500
+    SetAngleWheel( pw1, wheel )
+    SetAngleDoor( pw1, prim2, prim1 )
     #DisplayImg( image_file )
     #PlayAudio( sound_neutral )
---
+++ test_pigpio_servo.py
@@ -0,0 +1,51 @@
+# Test pigpio library to stop servo motor jitter
+
+import RPi.GPIO as GPIO
+import pigpio
+from time import sleep
+from PiFunctions import *
+
+pi = pigpio.pi()
+
+prim1 = 17
+prim2 = 27
+sec1 = 22
+sec2 = 10
+wheel = 9
+
+GPIO.setmode( GPIO.BCM )
+GPIO.setwarnings( False )     
+
+pi.set_servo_pulsewidth( prim1, 1500 )
+pi.set_servo_pulsewidth( prim2, 1500 )
+pi.set_servo_pulsewidth( sec1, 1500 )
+pi.set_servo_pulsewidth( sec2, 1500 )
+pi.set_servo_pulsewidth( wheel, 1500 )
+none = None
+btns = [ 14, 15, 18 ]
+check_mode = 0
+for i in range( len( btns ) ):
+    GPIO.setup( btns[ i ], GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+    GPIO.add_event_detect( btns[ i ], GPIO.RISING )
+while True:
+    if GPIO.event_detected( btns[ 0 ] ):
+        if check_mode == 0:
+            three_one( prim1, prim2, wheel, none, none )
+            check_mode = 1
+        if check_mode == 2:
+            two_one( sec1, sec2, none, none )
+            check_mode = 1
+    if GPIO.event_detected( btns[ 1 ] ):
+        if check_mode == 1:
+            one_two( sec1, sec2, none, none )
+            check_mode = 2
+        if check_mode == 3:
+            three_two( prim1, prim2, sec1, sec2, wheel, none, none )
+            check_mode = 2
+    if GPIO.event_detected( btns[ 2 ] ):
+        if check_mode == 2:
+            two_three( prim1, prim2, sec1, sec2, wheel, none, none )
+            check_mode = 3
+        if check_mode == 1:
+            one_three( prim1, prim2, wheel, none, none )
+            check_mode = 3
